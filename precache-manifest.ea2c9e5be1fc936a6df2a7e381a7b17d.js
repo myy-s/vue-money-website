8ce4f41e2f3dd16b21d5375c2211805d38612216
myy-s <1054165055@qq.com>
2020-05-02 03:06:57 +0800

    deploy

---
+++ precache-manifest.ea2c9e5be1fc936a6df2a7e381a7b17d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d8dab51b34c81a94af",
+    "url": "/vue-money-website/css/app.39669046.css"
+  },
+  {
+    "revision": "46506fc408098a8a5f64981a725ecc25",
+    "url": "/vue-money-website/index.html"
+  },
+  {
+    "revision": "b7d8dab51b34c81a94af",
+    "url": "/vue-money-website/js/app.01725d5e.js"
+  },
+  {
+    "revision": "d97eb894184ac1e1b1d3",
+    "url": "/vue-money-website/js/chunk-vendors.c2b5d7ae.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/vue-money-website/manifest.json"
+  },
+  {
+    "revision": "4dd846eab51547a1b3ff1e85f6c5afc0",
+    "url": "/vue-money-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-money-website/robots.txt"
+  }
+]);
